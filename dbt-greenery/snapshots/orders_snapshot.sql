d11074a1b8dd30a2dcd73c738781cabcbbf7ef31
Zach Renwick <zach.renwick@gmail.com>
2021-11-16 09:27:26 +0000

    adding orders snapshot (needs more work)

---
+++ dbt-greenery/snapshots/orders_snapshot.sql
@@ -0,0 +1,16 @@
+{% snapshot orders_snapshot %}
+
+  {{
+    config(
+      target_schema='snapshots',
+      unique_key='order_id',
+
+      strategy='timestamp',
+      updated_at='created_at'
+    )
+  }}
+
+  SELECT * 
+  FROM {{ source('stage', 'orders') }}
+
+{% endsnapshot %}
