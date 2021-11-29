5693ed15120561c0320ead96adb648a64c5fcbda
Zach Renwick <zach.renwick@gmail.com>
2021-11-29 05:57:58 +0000

    create custom schemas

--- dbt-greenery/models/marts/marketing/intermediate/int_user_order_totals.sql
+++ dbt-greenery/models/marts/marketing/intermediate/int_user_order_totals.sql
@@ -11,9 +11,11 @@ count(distinct order_id) AS order_count,
 SUM(order_cost) AS total_customer_cost,
 SUM(shipping_cost) AS total_shipping_cost,
 SUM(order_total) AS total_customer_revenue,
-count of products purchased
-first_order_date
-most_recent_order_date
+--count of products purchased
+MIN(created_at) AS first_order_date,
+MAX(created_at) AS most_recent_order_date
 
 FROM  {{ ref('dim_users') }}
-LEFT JOIN {{ ref('int_order_details') }} USING (user_id)
+LEFT OUTER JOIN {{ ref('int_order_details') }} USING (user_id)
+
+GROUP BY user_id
