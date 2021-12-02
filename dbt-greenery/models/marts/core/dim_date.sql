db6806c45556294514d673a3c606222fabe68b94
Zach Renwick <zach.renwick@gmail.com>
2021-12-02 04:30:27 +0000

    testing1

--- dbt-greenery/models/marts/core/dim_date.sql
+++ dbt-greenery/models/marts/core/dim_date.sql
@@ -19,5 +19,6 @@ WITH date_spine AS (
       date_day,
       EXTRACT( YEAR FROM date_day)       AS year,
       EXTRACT( MONTH FROM date_day)      AS month,
-      EXTRACT( DAY FROM date_day)        AS day
+      EXTRACT( DAY FROM date_day)        AS day,
+      1 as one
     FROM date_spine
