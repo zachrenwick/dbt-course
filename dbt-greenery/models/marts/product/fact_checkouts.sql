c2ec0110cebd8cf57ca511eb8925e3cb724ef05b
Zach Renwick <zach.renwick@gmail.com>
2021-12-07 06:55:29 +0000

    week3

--- dbt-greenery/models/marts/product/fact_checkouts.sql
+++ dbt-greenery/models/marts/product/fact_checkouts.sql
@@ -4,12 +4,13 @@
   )
 }}
 
-SELECT 
-  split_part(page_url, 'https://greenary.com/product/', 2) as product_id,
-  session_id,
-  DATE(created_at) as checkout_date,
-  EXTRACT(HOUR FROM created_at) as checkout_hour,
-  count(*) as checkout_count
+SELECT split_part(page_url, 'https://greenary.com/product/', 2) AS product_id
+	,session_id
+	,DATE (created_at) AS checkout_date
+	,EXTRACT(HOUR FROM created_at) AS checkout_hour
+	,count(*) AS checkout_count
 FROM {{ ref('stg_events') }}
 WHERE event_type = 'checkout'
+	AND page_url LIKE 'https://greenary.com/product/%'
+	AND created_at IS NOT NULL
 {{ dbt_utils.group_by(n=4) }}
